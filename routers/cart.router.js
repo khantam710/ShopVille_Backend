8f55cab99dffbe6ec7369e5363e6ae7117fe56b3
Tamanna Khan <134850822+khantam710@users.noreply.github.com>
2025-06-14 15:31:16 +0530

    Update cart.router.js
    Signed-off-by: Tamanna Khan <134850822+khantam710@users.noreply.github.com>

--- routers/cart.router.js
+++ routers/cart.router.js
@@ -1,6 +1,6 @@
 import express from 'express'
-import { getAllCarts, getUserCart, addCart, updateCart, deleteCart } from '../controllers/cart.controller'
-import { verifyAdmin, verifyUser } from '../auth'
+import { getAllCarts, getUserCart, addCart, updateCart, deleteCart } from '../controllers/cart.controller.js'
+import { verifyAdmin, verifyUser } from '../auth.js'
 const cartRouter = express.Router()
 
 cartRouter.post('/add/:userID',addCart)
